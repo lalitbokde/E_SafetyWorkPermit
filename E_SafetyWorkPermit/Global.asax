2f6503911808ae060556dd8f9e254c8b3d4d5078
lalit bokde <lalitthinker@gmail.com>
2018-09-01 08:51:32 -0700

    Upload

---
+++ E_SafetyWorkPermit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="E_SafetyWorkPermit.Global" Language="C#" %>
---
+++ E_SafetyWorkPermit/Models/VendorModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_SafetyWorkPermit.Models
+{
+    public class VendorRegistrationModel
+    {
+        public long VendorId { get; set; }
+        public string FullName { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int Status { get; set; }
+        public string CreatedDate { get; set; }
+        public string UpdatedDate { get; set; }
+
+    }
+    public class VendorRegistrationResponseModel
+    {
+        public long VendorId { get; set; }
+        public string FullName { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int Status { get; set; }
+        public string CreatedDate { get; set; }
+        public string UpdatedDate { get; set; }
+    }
+}
